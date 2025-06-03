a38f029beab4069f86cb74642df957f3fdfc42c5
Alessio-Civitella <alessio.civitella19@gmail.com>
2025-06-03 12:24:05 +0200

    Milestone 3

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 type Person = {
-  readonly id: number;           // identificativo non modificabile
-  readonly name: string;        // nome completo non modificabile
-  birth_year: number;           // anno di nascita
-  death_year?: number;          // anno di morte (opzionale)
-  biography: string;            // breve biografia
-  image: string;                // URL dell'immagine
+  readonly id: number;
+  readonly name: string;
+  birth_year: number;
+  death_year?: number;
+  biography: string;
+  image: string;
 };
 
 type Nationality =
@@ -21,7 +21,49 @@ type Nationality =
   | "Chinese";
 
 type Actress = Person & {
-  most_famous_movies: [string, string, string]; // tupla di 3 stringhe
+  most_famous_movies: [string, string, string];
   awards: string;
   nationality: Nationality;
 };
+
+function isActress(obj: any): obj is Actress {
+  return (
+    obj &&
+    typeof obj.id === "number" &&
+    typeof obj.name === "string" &&
+    typeof obj.birth_year === "number" &&
+    (typeof obj.death_year === "undefined" || typeof obj.death_year === "number") &&
+    typeof obj.biography === "string" &&
+    typeof obj.image === "string" &&
+    Array.isArray(obj.most_famous_movies) &&
+    obj.most_famous_movies.length === 3 &&
+    obj.most_famous_movies.every((movie: any) => typeof movie === "string") &&
+    typeof obj.awards === "string" &&
+    [
+      "American",
+      "British",
+      "Australian",
+      "Israeli-American",
+      "South African",
+      "French",
+      "Indian",
+      "Israeli",
+      "Spanish",
+      "South Korean",
+      "Chinese"
+    ].includes(obj.nationality)
+  );
+}
+
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const res = await fetch(`http://localhost:3333/actresses/${id}`);
+    if (!res.ok) return null;
+    const data = await res.json();
+    return isActress(data) ? data : null;
+  } catch (error) {
+    console.error("Errore nella richiesta:", error);
+    return null;
+  }
+}
